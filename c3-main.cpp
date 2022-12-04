34aa27cf4cc558e93e2613af409c47baefff07a5
jmh <>
2022-12-04 03:54:09 +0000

    fix typo and do actually processing

--- c3-main.cpp
+++ c3-main.cpp
@@ -60,7 +60,7 @@ Eigen::Matrix4d NDT(pcl::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointX
 
 PointCloudT::Ptr VoxelFilterCloud(PointCloudT::Ptr cloud){
 	pcl::VoxelGrid<PointT> vg;
-  	vg.setInputCloud(scans[0]);
+  	vg.setInputCloud(cloud);
   	vg.setLeafSize(filterRes, filterRes, filterRes);
 	typename pcl::PointCloud<PointT>::Ptr cloudFiltered (new pcl::PointCloud<PointT>);
   	vg.filter(*cloudFiltered);
@@ -238,15 +238,20 @@ int main(){
 			
 			new_scan = true;
 			// TODO: (Filter scan using voxel filter)
+			PointCloudT::Ptr cloudFiltered = VoxelFilterCloud(scanCloud);
 
 			// TODO: Find pose transform by using ICP or NDT matching
 			//pose = ....
+			Eigen::Matrix4d transform = NDT(ndt, cloudFiltered, pose, 3);
+			pose = getPose(transform);
 
 			// TODO: Transform scan so it aligns with ego's actual pose and render that scan
-// pcl::transformPointCloud (*cloudFiltered, *transformed_scan, transform);
+			PointCloudT::Ptr transformedCloudFiltered (new PointCloudT);
+			pcl::transformPointCloud (*cloudFiltered, *transformedCloudFiltered, transform);
+
 			viewer->removePointCloud("scan");
 			// TODO: Change `scanCloud` below to your transformed scan
-			renderPointCloud(viewer, scanCloud, "scan", Color(1,0,0) );
+			renderPointCloud(viewer, transformedCloudFiltered, "scan", Color(1,0,0) );
 
 			viewer->removeAllShapes();
 			drawCar(pose, 1,  Color(0,1,0), 0.35, viewer);
